a57d3cd09bef8e044eada7ba409f6e73611140d1
Jigyasa Khamele <24khamelej@rbunagpur.in>
2026-08-02 17:08:12 +0530

    Time: 5 ms (23.31%), Space: 43.1 MB (62.03%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int[] dp = piles.clone();
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                dp[i] = Math.max(piles[i] - dp[i + 1],
+                                 piles[j] - dp[i]);
+            }
+        }
+
+        return dp[0] > 0; 
+    }
+}
